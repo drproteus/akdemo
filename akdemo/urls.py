e7d085b50480cc25f67db4d23a732cc1f35e88db
Jake Goritski <jgoritski@gmail.com>
2020-11-24 17:08:31 -0500

    typo

--- akdemo/urls.py
+++ akdemo/urls.py
@@ -31,5 +31,5 @@ urlpatterns = [
     path('logout', public.views.Logout.as_view(), name="logout"),
     path('services/<slug:slug>', public.views.Service.as_view(), name="service"),
 
-    path('app/', main.views.Dashboard.as_views(), name="dashboard")
+    path('app/', main.views.Dashboard.as_view(), name="dashboard")
 ]
