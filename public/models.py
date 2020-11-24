331f331be569dc10ed9ef5c6dc0848bcfe8b6570
Jake Goritski <jgoritski@gmail.com>
2020-11-23 20:45:13 -0500

    checkpoint

--- public/models.py
+++ public/models.py
@@ -13,4 +13,19 @@ class Resource(models.Model):
     def __str__(self):
         return self.title
 
+
+class Service(models.Model):
+    title = models.CharField(max_length=256)
+    url = models.CharField(max_length=1024, blank=True)
+    description = models.TextField(blank=True)
+    parent = models.ForeignKey('Service', on_delete=models.CASCADE, related_name="children", blank=True, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
 admin.site.register(Resource)
+admin.site.register(Service)
